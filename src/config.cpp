e5fd7d452082b000d8e6a0fbbea2a813beb6688b
DgDamis <adam.smehyl@seznam.cz>
2020-03-01 17:55:21 +0100

    Broken

---
+++ src/config.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+constexpr uint16_t sleepTimeInSeconds = 300;
+//bool wifiConnection = false;
+//bool mqttConnection = false;
+//uint32_t mqtt_delay = 100;
+//uint16_t sleep_delay = 10000;
---
+++ src/credentials.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+//const char* ssid = "*************";
+//const char* password = "****************";
+//const char* mqtt_server = "******************";
+//const char* mqttClient_hostname = "xxxxxx";
+//const char* mqttClient_id = "xx";
+//const char* mqttClient_password = "xx";
