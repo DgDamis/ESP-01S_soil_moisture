ccce78a61e81d16eabade765855cb043ed31f51b
DgDamis <adam.smehyl@seznam.cz>
2020-03-01 19:07:57 +0100

    Baseline

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "config.cpp"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
