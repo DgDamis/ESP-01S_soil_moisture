3dd92ecbbb5e48562710b05f50a987cd7d635e75
DgDamis <adam.smehyl@seznam.cz>
2020-03-01 18:45:34 +0100

    Revert "Broken"
    This reverts commit e5fd7d452082b000d8e6a0fbbea2a813beb6688b.

--- src/config.cpp
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-constexpr uint16_t sleepTimeInSeconds = 300;
-//bool wifiConnection = false;
-//bool mqttConnection = false;
-//uint32_t mqtt_delay = 100;
-//uint16_t sleep_delay = 10000;
